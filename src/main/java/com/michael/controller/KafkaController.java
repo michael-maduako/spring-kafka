f4c9732e791b58b146b4f3ff3008684c2ae7da37
mboy4 <mboy4@DESKTOP-4AT5QTM>
2020-05-08 23:50:01 -0400

    Now returning an Object instead of a String
    Added "Kafkaconfiguration" class and "User" class in order to allow the
    program to return an Object instead of just a string.

--- src/main/java/com/michael/controller/KafkaController.java
+++ src/main/java/com/michael/controller/KafkaController.java
@@ -1,6 +1,8 @@
 package com.michael.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,15 +14,34 @@ import com.michael.service.KafkaSender;
 public class KafkaController {
 	@Autowired
 	KafkaSender kafkaSender;
-	@RequestMapping("/")
+
+	@GetMapping("/")
 	public String display() {
 		return "Welcome to springboot kafka services";
 	}
 	//localhost:8080/producer?msg="abc"
-	@RequestMapping("/producer")
-	public String producer(@RequestParam("msg")String msg) {
+	@GetMapping("/producer")
+	public String producer(@RequestParam("msg") String msg) {
 		kafkaSender.send(msg);
 		return "Message sent to the kafka topic successfully";
 	}
+	
+	
+	/**
+	 * @Autowired
+	 * KafkaTemplate<String,String> kT;
+	 * 
+	 * private static final String TOPIC="Kafka_Example";
+	 * 
+	 * @GetMapping("/publish/{msg}")
+	 * public String psot(@PathVariable("msg") final String msg){
+	 * 	kT.send(TOPIC, msg);
+	 * 	return "Published successfully";
+	 * }
+	 * 
+	 * 
+	 * */
+	
+	
 	
 }
--- src/main/java/com/michael/service/KafkaSender.java
+++ src/main/java/com/michael/service/KafkaSender.java
@@ -4,13 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import com.michael.model.User;
+
 @Service
 public class KafkaSender {
 	@Autowired
-	private KafkaTemplate<String, String> kafka;
-	String topicName = "sam-topic";
+	private KafkaTemplate<String, User> kafkaTemplate;
+	
+	private static final String topicName = "sam-topic";
 
-	public void send(String msg) {
-		kafka.send(topicName,msg);
+	public void send(String name) {
+		kafkaTemplate.send(topicName,new User(name,"Web developer",20000L));
+		
 	}
 }
